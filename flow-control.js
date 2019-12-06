5677048754fc6293c827306b52cb220a88790be8
Jim Rountree <jimvrountree@gmail.com>
2019-12-06 15:48:02 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -26,7 +26,10 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
+  conditionToTest ? (13 <= age && age <= 19)  
 
+} return "You are a teenager" {
+  
 }
 
 function switchAge(age) {
